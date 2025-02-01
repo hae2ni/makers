1bf6adab8c1300af0632ac89725bca06f5fd5806
HAE2NI <hae0_02ni@ewhain.net>
2025-02-01 17:20:54 +0900

    💄 design: add theme.ts (아직 고민중,,,)

---
+++ assignment/src/styles/theme.ts
@@ -0,0 +1,13 @@
+const colors = {
+  background: "#FBFBFB",
+  lightblue: "#E8F9FF",
+  second: "#C4D9FF",
+  primary: "#C5BAFF",
+};
+
+const fonts = {};
+
+export const Theme = {
+  colors: colors,
+  fonts: fonts,
+};
