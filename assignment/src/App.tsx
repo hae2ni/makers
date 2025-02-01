e15224e4f0147a265e46fbb5c6fce3b29227ffbb
HAE2NI <hae0_02ni@ewhain.net>
2025-02-01 17:10:56 +0900

    💄 design: add globlStyle.ts

--- assignment/src/App.tsx
+++ assignment/src/App.tsx
@@ -1,5 +1,5 @@
 function App() {
-  return <></>;
+  return <>dd</>;
 }
 
 export default App;
---
+++ assignment/src/styles/globalStyle.ts
@@ -0,0 +1,39 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+export const globlStyles = createGlobalStyle`
+${reset}
+* {
+  box-sizing: border-box;
+}
+
+html,
+body {
+  width: 100%;
+  height: 100vh;
+  margin: 0 auto;
+  font-size: 62.5%
+}
+
+a {
+  color: inherit;
+  text-decoration: none;
+  cursor: pointer;
+}
+
+button {
+  border: none;
+  background: none;
+  font: inherit;
+  cursor: pointer;
+}
+
+html {
+    @font-face {
+    font-family: 'Pretendard-Regular';
+    src: url('https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
+    font-weight: 400;
+    font-style: normal;
+}
+}
+`;
