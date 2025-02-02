1c70cf19d924cf997c1a35d715a7955c93d8ffe6
HAE2NI <hae0_02ni@ewhain.net>
2025-02-02 21:07:11 +0900

    🎨 style: add ThemeProvider

--- assignment/src/App.tsx
+++ assignment/src/App.tsx
@@ -1,5 +1,17 @@
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "./styles/globalStyle";
+import { Theme } from "./styles/theme";
+import Home from "./pages/Home";
+
 function App() {
-  return <>dd</>;
+  return (
+    <>
+      <ThemeProvider theme={Theme}>
+        <GlobalStyles />
+        <Home />
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default App;
