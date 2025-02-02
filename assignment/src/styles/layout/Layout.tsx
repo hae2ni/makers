566e1ff5e8b54c6eb931e5cbec6f548429ae7ab2
HAE2NI <hae0_02ni@ewhain.net>
2025-02-02 21:06:26 +0900

    💄 design: add layout component

---
+++ assignment/src/styles/layout/Layout.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export default function Layout() {
+  return <LayoutContainer>Layout</LayoutContainer>;
+}
+
+const LayoutContainer = styled.div`
+  width: "144rem";
+  height: "120rem";
+`;
