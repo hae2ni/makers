252c546b5a317751e7f5a997900fa55bcfc89071
HAE2NI <hae0_02ni@ewhain.net>
2025-02-03 17:54:16 +0900

    🚑 fix: fix error of commonStyle

--- assignment/src/styles/commonStyles.ts
+++ assignment/src/styles/commonStyles.ts
@@ -1,14 +1,14 @@
 import { css } from "styled-components";
 
 export const flex = css`
-  display: "flex";
-  justify-content: "center";
-  align-items: "center";
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 export const columnflex = css`
-  display: "flex";
-  flex-direction: "column";
-  justify-content: "center";
-  align-items: "center";
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
