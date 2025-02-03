3a26c42f7fcf077fae0c9e5019d7f372bf09e191
HAE2NI <hae0_02ni@ewhain.net>
2025-02-03 15:05:53 +0900

    🎨 feat: add outlet

--- assignment/src/styles/layout/Layout.tsx
+++ assignment/src/styles/layout/Layout.tsx
@@ -1,10 +1,28 @@
 import styled from "styled-components";
+import TabComponent from "../../components/TabComponent";
+import { Outlet } from "react-router";
 
 export default function Layout() {
-  return <LayoutContainer>Layout</LayoutContainer>;
+  return (
+    <LayoutContainer>
+      <TabComponent />
+      <main>
+        <Outlet />
+      </main>
+    </LayoutContainer>
+  );
 }
 
 const LayoutContainer = styled.div`
-  width: "144rem";
-  height: "120rem";
+  width: 80%;
+  margin: 0 auto;
+
+  @media (max-width: 768px) {
+    width: 100%;
+    padding: 2rem;
+  }
+
+  @media (max-width: 480px) {
+    padding: 1rem;
+  }
 `;
