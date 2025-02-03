022055293a9dd30840d0610c924be3ffa99a3c02
HAE2NI <hae0_02ni@ewhain.net>
2025-02-03 17:54:31 +0900

    ✨ feat: import DateCompo

---
+++ assignment/src/pages/Future.tsx
@@ -0,0 +1,10 @@
+import SelectDateComponent from "../components/SelectDateComponent";
+import useFuture from "../hooks/useFuture";
+
+export default function Future() {
+  const seoul = "Seoul";
+  const dt = "2025-12-25";
+  const { data } = useFuture({ location: seoul, date: dt });
+  console.log(data);
+  return <SelectDateComponent />;
+}
