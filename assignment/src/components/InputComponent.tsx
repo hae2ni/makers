3196f73ce01236a15a97c7b58ce6cfed57e6aad2
HAE2NI <hae0_02ni@ewhain.net>
2025-02-03 15:05:30 +0900

    ✨ feat: input component

---
+++ assignment/src/components/InputComponent.tsx
@@ -0,0 +1,34 @@
+import { useRef, useState } from "react";
+import { useCityStore } from "../store/useCityStore";
+
+//사용자 입력을 받는 입력 필드 컴포넌트입니다!
+export default function InputComponent() {
+  const { setCity } = useCityStore();
+  const inputRef = useRef(null);
+  const [value, setValue] = useState("");
+  const [disabled, setDisabled] = useState(false);
+  const checkEng = /[a-zA-Z]/;
+
+  function handleOnChangeInput(e: React.ChangeEvent<HTMLInputElement>) {
+    setValue(e.target.value);
+    if (!checkEng.test(e.target.value)) {
+      setDisabled(true);
+    } else {
+      setDisabled(false);
+    }
+  }
+
+  function handleOnClickButton() {
+    setCity(value);
+  }
+
+  return (
+    <>
+      <input value={value} type="search" ref={inputRef} onChange={handleOnChangeInput} />
+      <button onClick={handleOnClickButton} disabled={disabled}>
+        Enter
+      </button>
+      {disabled && <p>Please enter only Eng</p>}
+    </>
+  );
+}
