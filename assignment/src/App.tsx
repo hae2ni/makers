7db537cd844c5a245b02345a82ff7882c9f1f4ab
HAE2NI <hae0_02ni@ewhain.net>
2025-02-03 16:36:55 +0900

    ✨ feat: route

--- assignment/src/App.tsx
+++ assignment/src/App.tsx
@@ -1,14 +1,29 @@
 import { ThemeProvider } from "styled-components";
 import { GlobalStyles } from "./styles/globalStyle";
 import { Theme } from "./styles/theme";
+import { BrowserRouter, Route, Routes } from "react-router";
+import Layout from "./styles/layout/Layout";
+import Current from "./pages/Current";
+import Forecast from "./pages/Forecast";
+import Future from "./pages/Future";
 import Home from "./pages/Home";
+import "@sopt-makers/ui/dist/index.css";
 
 function App() {
   return (
     <>
       <ThemeProvider theme={Theme}>
         <GlobalStyles />
-        <Home />
+        <BrowserRouter>
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Home />} />
+              <Route path="current" element={<Current />} />
+              <Route path="forecast" element={<Forecast />} />
+              <Route path="future" element={<Future />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
       </ThemeProvider>
     </>
   );
