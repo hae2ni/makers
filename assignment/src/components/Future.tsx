e30cba6c3c33cb25bbaf0c77a74655de2aa890c3
HAE2NI <hae0_02ni@ewhain.net>
2025-02-03 16:37:18 +0900

    🚚 move: move file

--- assignment/src/components/Future.tsx
+++
@@ -1,9 +0,0 @@
-import useFuture from "../hooks/useFuture";
-
-export default function Future() {
-  const seoul = "Seoul";
-  const dt = "2025-12-25";
-  const { data } = useFuture({ location: seoul, date: dt });
-  console.log(data);
-  return <div>Future</div>;
-}
