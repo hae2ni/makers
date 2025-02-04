ba23565c5906b00f478c1fb076d76bf3d3367a36
HAE2NI <hae0_02ni@ewhain.net>
2025-02-04 13:24:39 +0900

    💄 design: add Skeleton and Callout Component for loading and error pacg each, in mds

---
+++ assignment/src/pages/Error.tsx
@@ -0,0 +1,5 @@
+import { Callout } from "@sopt-makers/ui";
+
+export default function Error() {
+  return <Callout type="danger">Error</Callout>;
+}
--- assignment/src/pages/Loading.tsx
+++ assignment/src/pages/Loading.tsx
@@ -1,3 +1,10 @@
+import { Skeleton } from "@sopt-makers/ui";
+
+//로딩뷰,,, mds 스캘래톤 빌려 썼습니다!!
 export default function Loading() {
-  return <div>Loading</div>;
+  return (
+    <>
+      <Skeleton height={100} width={100} variant="default" />
+    </>
+  );
 }
