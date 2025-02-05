bd8e1abe1e0edc7f588b0dae23a325ef5f926a49
HAE2NI <hae0_02ni@ewhain.net>
2025-02-05 15:26:41 +0900

    ✨ feat: add SWRConfig

--- assignment/src/main.tsx
+++ assignment/src/main.tsx
@@ -1,9 +1,20 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import { SWRConfig } from "swr";
+import Error from "./pages/Error.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>
+    <SWRConfig
+      value={{
+        onError: (error) => {
+          if (error.status == 400) {
+            return <Error />;
+          }
+        },
+      }}>
+      <App />
+    </SWRConfig>
+  </StrictMode>,
 );
