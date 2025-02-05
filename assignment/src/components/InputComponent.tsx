32a5b7209b1c79a82bc628b0e025cf2c939e8e96
HAE2NI <hae0_02ni@ewhain.net>
2025-02-05 15:28:03 +0900

    ✨ feat: add useEffect to when you erase all of your typings, just alter disable

--- assignment/src/components/InputComponent.tsx
+++ assignment/src/components/InputComponent.tsx
@@ -1,5 +1,9 @@
-import { useRef, useState } from "react";
+fimport { useEffect, useRef, useState } from "react";
 import { useCityStore } from "../store/useCityStore";
+import styled from "styled-components";
+import { columnflex, flex } from "../styles/commonStyles";
+import { colors } from "@sopt-makers/colors";
+import { fontsObject } from "@sopt-makers/fonts";
 
 //사용자 입력을 받는 입력 필드 컴포넌트입니다!
 export default function InputComponent() {
@@ -9,11 +13,18 @@ export default function InputComponent() {
   const [disabled, setDisabled] = useState(false);
   const checkEng = /[a-zA-Z]/;
 
+  useEffect(() => {
+    if (value.length == 0) {
+      setDisabled(false);
+    }
+  }, [value]);
+
   function handleOnChangeInput(e: React.ChangeEvent<HTMLInputElement>) {
     setValue(e.target.value);
+
     if (!checkEng.test(e.target.value)) {
       setDisabled(true);
-    } else {
+    } else if (checkEng.test(e.target.value)) {
       setDisabled(false);
     }
   }
@@ -28,12 +39,55 @@ export default function InputComponent() {
   }
 
   return (
-    <>
-      <input value={value} type="search" ref={inputRef} onChange={handleOnChangeInput} />
-      <button onClick={handleOnClickButton} disabled={disabled}>
-        Enter
-      </button>
-      {disabled && <p>Please enter only Eng</p>}
-    </>
+    <Container>
+      <InputContianer>
+        <Input value={value} type="search" ref={inputRef} onChange={handleOnChangeInput} />
+        <Button onClick={handleOnClickButton} disabled={disabled}>
+          Enter
+        </Button>
+      </InputContianer>
+      {disabled && <Alert>Please enter only Eng</Alert>}
+    </Container>
   );
 }
+
+const Container = styled.div`
+  ${columnflex}
+  gap: 1rem;
+  align-items: flex-start;
+`;
+
+const InputContianer = styled.div`
+  ${flex}
+  gap: 0.2rem
+`;
+
+const Input = styled.input`
+  ${flex}
+  justify-content: flex-start;
+
+  width: 35rem;
+  height: 5rem;
+
+  border-radius: 12px;
+  border-color: ${colors.blue700};
+  padding-left: 0.5rem;
+  border-style: solid;
+
+  margin: 0;
+`;
+
+const Button = styled.button`
+  ${fontsObject.BODY_3_14_M}
+
+  &:hover {
+    color: ${colors.blue600};
+  }
+`;
+
+const Alert = styled.p`
+  ${fontsObject.LABEL_5_11_SB}
+  color: ${colors.red200};
+
+  padding-left: 0.5rem;
+`;
