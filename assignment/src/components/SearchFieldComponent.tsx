4eab7162b5dbf04bfd70a49e221f7a59c4b0cc4b
HAE2NI <hae0_02ni@ewhain.net>
2025-02-05 17:13:53 +0900

    🔥 remove: delete code

--- assignment/src/components/SearchFieldComponent.tsx
+++
@@ -1,9 +0,0 @@
-import { SearchField } from "@sopt-makers/ui";
-export default function SearchFieldComponent() {
-  function handleSubmit() {}
-  function handleReset() {}
-
-  return (
-    <SearchField placeholder="Please enter the name of city" value="" onSubmit={handleSubmit} onReset={handleReset} />
-  );
-}
