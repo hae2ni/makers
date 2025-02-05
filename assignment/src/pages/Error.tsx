8fcff34b61a0a005118c5be223bc0a9af0d88ca1
HAE2NI <hae0_02ni@ewhain.net>
2025-02-05 15:27:20 +0900

    ✨ feat: make error page

--- assignment/src/pages/Error.tsx
+++ assignment/src/pages/Error.tsx
@@ -1,5 +1,35 @@
+import { colors } from "@sopt-makers/colors";
+import { fontsObject } from "@sopt-makers/fonts";
 import { Callout } from "@sopt-makers/ui";
+import { useNavigate } from "react-router";
+import styled from "styled-components";
 
 export default function Error() {
-  return <Callout type="danger">Error</Callout>;
+  const navigation = useNavigate();
+
+  return (
+    <Callout hasIcon type="danger">
+      <CalloutStyle>
+        Please enter the correct city name or press the sentence behind <br />
+        It will display Seoul as the default
+      </CalloutStyle>
+      <ButtonText onClick={() => location.reload()}>Change to the default option, Seoul</ButtonText>
+      <ButtonText onClick={() => navigation("/")}>Click here to change it to the correct city name</ButtonText>
+    </Callout>
+  );
 }
+
+const CalloutStyle = styled.p`
+  ${fontsObject.TITLE_5_18_SB}
+  color: ${colors.black};
+  margin-bottom: 2rem;
+`;
+
+const ButtonText = styled.button`
+  ${fontsObject.LABEL_3_14_SB}
+  color: ${colors.gray600};
+
+  &:hover {
+    color: ${colors.blue500};
+  }
+`;
